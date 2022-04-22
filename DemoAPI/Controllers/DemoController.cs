662c5c2a4ed41d844ec341a0b46a0a73f7f8483f
Yogeswari <yogeswari@graph1test.onmicrosoft.com>
2022-04-22 12:52:29 +0000

    API

---
+++ DemoAPI/Controllers/DemoController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DemoApp.Models;
+using Microsoft.AspNetCore.Cors;
+
+namespace DemoApp.Controllers
+{
+    [Route("api/Demo")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            List<DemoSell> lst = new List<DemoSell>();
+            lst.Add(new DemoSell { date = Convert.ToDateTime("12/12/12"), fuelType = "Gas", price = Convert.ToDouble(10) });
+            lst.Add(new DemoSell { date = Convert.ToDateTime("11/12/12"), fuelType = "Gas", price = Convert.ToDouble(10) });
+            lst.Add(new DemoSell { date = Convert.ToDateTime("10/12/12"), fuelType = "Gas", price = Convert.ToDouble(10) });
+
+            // return  lst.ToArray();
+            return new JsonResult(lst);
+        }
+
+        [HttpPost]
+        public JsonResult Post(DemoSell demoSell)
+        {
+            List<DemoSell> lst = new List<DemoSell>();
+            demoSell.discountedPrice = discountedPrice(demoSell);
+            lst.Add(demoSell);
+            return new JsonResult(lst);
+
+        }
+        private double discountedPrice(DemoSell demoSell)
+        {
+            double price = 0;
+            if (demoSell != null)
+            {
+                DayOfWeek day = demoSell.date.DayOfWeek;
+                if ((day == DayOfWeek.Saturday) || (day == DayOfWeek.Sunday))
+                {
+                    price = demoSell.price - (demoSell.price * .1);
+                }
+                else
+                {
+                    price = demoSell.price;
+                }
+            }
+            return price;
+        }
+
+    }
+}
---
+++ DemoAPI/Models/DemoSell.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoApp.Models
+{
+    public class DemoSell
+    {
+        public DateTime date { get; set; }
+        public string fuelType { get; set; }
+        public Double price { get; set; }
+        public double? discountedPrice { get; set; }
+    }
+}
